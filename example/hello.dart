fd608e7f7f79b3b7c5d8828bd5b8ddcff3a78ad0
rafli <rafli@mediainovasi.id>
2023-11-23 14:58:41 +0700

    Initial commit

---
+++ example/hello.dart
@@ -0,0 +1,10 @@
+import 'package:belajar_dart_packages_library/hello.dart';
+
+void main() {
+
+  print(sayHello("Rafli"));
+
+  var customer = Customer();
+  var product = Product();
+
+}
---
+++ lib/hello.dart
@@ -0,0 +1,4 @@
+library belajar_dart_package_library;
+
+export 'src/say_hello.dart';
+export 'src/customer.dart' show Customer, Product;
